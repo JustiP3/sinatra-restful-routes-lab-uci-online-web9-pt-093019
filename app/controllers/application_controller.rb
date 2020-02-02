1099bb17ebda297bdb191849769f59aa0080ad8b
Justin Phillips <justinphil02@yahoo.com>
2020-02-02 04:26:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < Sinatra::Base
   patch '/recipes/:id' do 
     @recipe = Recipe.find(params[:id])
     @recipe.ingredients = params[:ingredients]
+    @recipe.name = params[:name]
     @recipe.save
   end 
 
