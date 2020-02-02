2f368b54e17f7cfb580839a8a5a6711783bc02ad
Justin Phillips <justinphil02@yahoo.com>
2020-02-02 04:23:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/recipes/' do
+  post '/recipes' do
     @recipe = Recipe.new(params)
     if @recipe.save    
       binding.pry
