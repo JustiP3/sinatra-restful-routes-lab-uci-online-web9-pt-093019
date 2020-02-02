6dfaf6552d850e67a345fa26e8bd870a50c1780d
Justin Phillips <justinphil02@yahoo.com>
2020-02-02 03:13:13 +0000

    Automatically backed up by Learn

--- db/migrate/20200202031149_create_recipes.rb
+++ db/migrate/20200202031149_create_recipes.rb
@@ -1,6 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
-    def create_table |t|
+    def create_table do |t|
       t.string :name
       t.string :ingredients
       t.integer :cook_time
